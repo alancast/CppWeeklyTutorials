9a3d3540d579528b502c6767991b715ca5548629
Alex Lancaster <alancast@umich.edu>
2018-03-06 10:14:43 -0800

    first commit

---
+++ LearningModernCpp/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "testing a really long string testing a really long string "
+                 "testing a really long string \n";
+    std::cout << "testing\n";
+    std::cout << "Hello world\n";
+}
